e01e243690a41cb45918ea990ceb67030409c6c9
ItzMaze <artnik349@gmail.com>
2025-01-25 14:00:07 +0200

    initial commit and first screen

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./*/*.html", "./*/*/*.html", "./*/*/*/*.html", "./*/*/*/*/*.html"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  theme: {
+    screens: {
+      xl: { max: "1440px" },
+      lg: { max: "1024px" },
+      md: { max: "768px" },
+      sm: { max: "380px" },
+    },
+    extend: {
+      colors: {
+        yellow: "#FDCE40",
+        blue: "#009FE2",
+      },
+    },
+  },
+};
