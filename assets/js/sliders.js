5f39703e58ae8b6e5ce8f07875f32de9574fbf82
ItzMaze <artnik349@gmail.com>
2025-01-27 19:48:29 +0200

    Mobile addaptive

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -1,8 +1,13 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
+  slidesPerView: 2,
   spaceBetween: 14,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
+    },
   },
 });
